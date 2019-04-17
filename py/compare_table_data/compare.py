2d54e98eedfefc82c46d33e3cd5cc8b7231f5884
Stephen Roman <sroman@lilliirnb.com>
2019-04-17 13:34:13 -0400

    add logfile output, begin '--administrative' option

--- py/compare_table_data/compare.py
+++ py/compare_table_data/compare.py
@@ -1,3 +1,5 @@
+import sys
+
 import cx_Oracle
 import argparse
 import jprops
@@ -9,15 +11,17 @@ def trace(logged_func):
     Only works when decorating methods for LoggedObjects because the
     first arg must be the object being logged (a subclass of LoggedObject)
     """
+
     def wrapper(*args, **kwargs):
         print(f'entering function \'{logged_func.__name__}\' with args {args}')
         val = logged_func(*args, **kwargs)
         print(f'exited function \'{logged_func.__name__}\' with return value \'{val}\'')
         return val
+
     return wrapper
 
 
-def connect(username: str, password: str, hostname: str, db_name: str, port_no: str):
+def connect(username: str, password: str, hostname: str, db_name: str, port_no: str, logfile=sys.stdout):
     """
     :param username:
     :param password:
@@ -27,11 +31,11 @@ def connect(username: str, password: str, hostname: str, db_name: str, port_no: 
     :return: a Connection object
     """
     conn_str = cx_Oracle.makedsn(host=hostname, port=port_no, sid=db_name)
-    print(f'connecting to DB with connection string: {conn_str}')
+    print(f'connecting to DB with connection string: {conn_str}', file=logfile)
     return cx_Oracle.connect(username, password, conn_str)
 
 
-def execute_query(conn, statement: str, debug=False, **kwargs):
+def execute_query(conn, statement: str, debug=False, logfile=sys.stdout, **kwargs):
     """
     :param conn:
     :param statement:
@@ -40,7 +44,7 @@ def execute_query(conn, statement: str, debug=False, **kwargs):
     :return: a Cursor object
     """
     if debug:
-        print(f'executing statement: {statement} with positional args {kwargs}')
+        print(f'executing statement: {statement} with positional args {kwargs}', file=logfile)
     cursor = conn.cursor()
     cursor.execute(statement, kwargs)
     return cursor
@@ -115,7 +119,7 @@ def find_pk_constraint_for_table(conn, table: str, owner_user: str):
     return tuple([col[0] for col in execute_query(conn, stmt, tab=table, owner_user=owner_user)])
 
 
-def subtract_row_sets(conn1, conn2, table: str, cols: tuple, reverse=False):
+def subtract_row_sets(conn1, conn2, table: str, cols: tuple, reverse=False, logfile=sys.stdout):
     """
     Compare the data in @table from @conn1 and @conn2 by columns @cols
     :param table:
@@ -150,7 +154,7 @@ def subtract_row_sets(conn1, conn2, table: str, cols: tuple, reverse=False):
         t2_data = set(c1.execute(select_all).fetchmany(numRows=total_rows))
     except cx_Oracle.DatabaseError as e:
         if 'ORA-00942' in str(e):
-            print(f'table {table} does not exist in DB2, comparison cannot continue')
+            print(f'table {table} does not exist in DB2, comparison cannot continue', file=logfile)
             return
         else:
             raise e
@@ -160,15 +164,58 @@ def subtract_row_sets(conn1, conn2, table: str, cols: tuple, reverse=False):
     return tuple(t1_data - t2_data)
 
 
-def print_record(table1: str, table2: str, record):
-    """
-    print a formatted record in @table1 that's not in @table2
-    :param user:
-    :param table:
-    :param record:
-    :return:
-    """
-    pass
+def compare_table_data(args, logfile):
+
+    conn1 = connect(
+        username=args.username1,
+        password=args.passwd1,
+        hostname=args.hostname1,
+        db_name=args.db_name1,
+        port_no=args.port_no1,
+        logfile=logfile
+    )
+
+    conn2 = connect(
+        username=args.username2,
+        password=args.passwd2,
+        hostname=args.hostname2,
+        db_name=args.db_name2,
+        port_no=args.port_no2,
+        logfile=logfile
+    )
+
+    user_tables = [result_row[0] for result_row in select_table_names_for_user(conn1, args.username1.upper())]
+    for t in user_tables:
+        pk = get_pk_for_table(conn1, t, args.username1.upper())
+        if pk:  # filter tables that have no PK constraint or columns that begin with "ID"
+            print(f'========== {t} ==========', file=logfile)
+            diff_rows = subtract_row_sets(conn1, conn2, t, pk, args.reverse, logfile=logfile)
+            if diff_rows:
+                for row in diff_rows:
+                    print(row, file=logfile)
+        else:
+            print(f'table {t} has no primary key constraint or column that begins with \'ID_\'', file=logfile)
+
+
+def compare_admin_cfgs(args, logfile):
+
+    conn1 = connect(
+        username=args.admin_username1,
+        password=args.admin_passwd1,
+        hostname=args.hostname1,
+        db_name=args.db_name1,
+        port_no=args.port_no1,
+        logfile=logfile
+    )
+
+    conn2 = connect(
+        username=args.admin_username2,
+        password=args.admin_passwd2,
+        hostname=args.hostname2,
+        db_name=args.db_name2,
+        port_no=args.port_no2,
+        logfile=logfile
+    )
 
 
 def main():
@@ -185,60 +232,65 @@ def main():
     parser.add_argument('username2', help='The name of the user to connect to \'db_name2\' with')
     parser.add_argument('passwd2', help='Password for the user supplied by \'username2\'')
 
-    parser.add_argument('-r', '--reverse',
-                        help='Find data in DB2 not in DB1, the opposite of a normal run',
-                        action='store_true')
+    parser.add_argument('admin_username1', help='Username for the administrator user for DB1')
+    parser.add_argument('admin_passwd1', help='Password for user \'admin_username1\'')
+    parser.add_argument('admin_username2', help='Username for the administrator user for DB2')
+    parser.add_argument('admin_passwd2', help='Password for user \'admin_username2\'')
 
-    try:
-        with open('dbs.properties') as p:
-            properties = jprops.load_properties(p)
+    opts = parser.add_mutually_exclusive_group()
+    opts.add_argument('-r', '--reverse',
+                      help='Find data in DB2 not in DB1, the opposite of a normal run',
+                      action='store_true')
 
-            username1 = properties['db1.username']
-            password1 = properties['db1.passwd']
-            hostname1 = properties['db1.hostname']
-            dbname1 = properties['db1.db_name']
-            port_no1 = properties['db1.port_no']
+    opts.add_argument('-x', '--administrative',
+                      help='Run comparison on roles, privileges and other administrative '
+                           'configurations instead of regular table data',
+                      action='store_true')
 
-            username2 = properties['db2.username']
-            password2 = properties['db2.passwd']
-            hostname2 = properties['db2.hostname']
-            dbname2 = properties['db2.db_name']
-            port_no2 = properties['db2.port_no']
+    with open('./out.log', 'w') as logfile:
+        try:
+            with open('dbs.properties') as p:
+                properties = jprops.load_properties(p)
 
-            args = parser.parse_args([
-                hostname1, dbname1, port_no1, username1, password1,
-                hostname2, dbname2, port_no2, username2, password2,
-            ])
-    except Exception:
-        print('no file detected, using CLI input')
-        args = parser.parse_args()
+                username1 = properties['db1.username']
+                password1 = properties['db1.passwd']
+                hostname1 = properties['db1.hostname']
+                dbname1 = properties['db1.db_name']
+                port_no1 = properties['db1.port_no']
 
-    conn1 = connect(
-        username=args.username1,
-        password=args.passwd1,
-        hostname=args.hostname1,
-        db_name=args.db_name1,
-        port_no=args.port_no1
-    )
+                username2 = properties['db2.username']
+                password2 = properties['db2.passwd']
+                hostname2 = properties['db2.hostname']
+                dbname2 = properties['db2.db_name']
+                port_no2 = properties['db2.port_no']
 
-    conn2 = connect(
-        username=args.username2,
-        password=args.passwd2,
-        hostname=args.hostname2,
-        db_name=args.db_name2,
-        port_no=args.port_no2)
+                reverse = properties['reverse']
+                administrative = properties['administrative']
 
-    user_tables = [result_row[0] for result_row in select_table_names_for_user(conn1, args.username1.upper())]
-    for t in user_tables:
-        pk = get_pk_for_table(conn1, t, args.username1.upper())
-        if pk:  # filter tables that have no PK constraint or columns that begin with "ID"
-            print(f'========== {t} ==========')
-            diff_rows = subtract_row_sets(conn1, conn2, t, pk, args.reverse)
-            if diff_rows:
-                for row in diff_rows:
-                    print(row)
+                admin_username1 = properties['db1.admin_username']
+                admin_passwd1 = properties['db1.admin_passwd']
+
+                admin_username2 = properties['db2.admin_username']
+                admin_passwd2 = properties['db2.admin_passwd']
+
+                # fixme allow for options when using file for input
+                args = parser.parse_args([
+                    hostname1, dbname1, port_no1, username1, password1,
+                    hostname2, dbname2, port_no2, username2, password2,
+                    admin_username1, admin_passwd1,
+                    admin_username2, admin_passwd2,
+                    # '--reverse' if reverse else None,
+                    # '--administrative' if administrative else None
+                ])
+        except Exception as e:
+            print('no file detected, using CLI input', file=logfile)
+            print(str(e))
+            args = parser.parse_args()
+
+        if args.administrative:
+            compare_admin_cfgs(args, logfile)
         else:
-            print(f'table {t} has no primary key constraint or column that begins with \'ID_\'')
+            compare_table_data(args, logfile)
 
 
 if __name__ == '__main__':
