91c638be13819e93e2b6b50bb561f74cd18d3813
dolmengate <stephenjroman@protonmail.com>
2019-04-19 22:42:18 -0400

    move workstation provisioning scripts to dedicated repo

--- bash/machine/inst-rvm.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-sudo apt-add-repository -y ppa:rael-gc/rvm 
-sudo apt-get update 
-sudo apt-get install rvm 
-sudo useradd -G rvm $(whoami)
-echo ". /etc/profile.d/rvm.sh" >> ~/.bashrc
-echo "RVM installed, you must logout and log back in to use RVM"
-
